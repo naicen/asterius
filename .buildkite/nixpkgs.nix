b6a3953eb064c132211f39c81f667592abd7851c
Shao Cheng <cheng.shao@tweag.io>
2020-05-13 14:09:30 +0200

    WIP

--- .buildkite/nixpkgs.nix
+++ .buildkite/nixpkgs.nix
@@ -1,6 +1,6 @@
 let
-  rev = "5f14d99efed32721172a819b6e78a5520bab4bc6";
-  sha256 = "1nxqbcsc8bfmwy450pv6s12nbvzqxai5mr6v41y478pya26lb108";
+  rev = "6bcb1dec8ea16f20e6404631668cf69e76424eef";
+  sha256 = "04x750byjr397d3mfwkl09b2cz7z71fcykhvn8ypxrck8w7kdi1h";
 in
 import (fetchTarball {
   inherit sha256;
