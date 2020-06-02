a085427a6dbc539497f7af5716d8efcdd265739a
Shao Cheng <cheng.shao@tweag.io>
2020-06-02 19:04:41 +0200

    WIP

---
+++ .github/workflows/build-musl.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -eu
+
+echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+apk update --no-progress
+apk upgrade --no-progress
+apk add --no-progress \
+  alpine-sdk \
+  autoconf \
+  automake \
+  binutils-gold \
+  bzip2 \
+  coreutils \
+  curl \
+  file \
+  findutils \
+  g++ \
+  gawk \
+  ghc \
+  git \
+  gzip \
+  libtool \
+  musl-dev \
+  ncurses-dev \
+  numactl-dev \
+  openssh \
+  patch \
+  py3-sphinx \
+  sed \
+  tar \
+  xz \
+  zlib-dev
+mkdir -p ~/.local/bin
+curl -L https://github.com/commercialhaskell/stack/releases/download/v2.3.1/stack-2.3.1-linux-x86_64-bin -o ~/.local/bin/stack
+chmod u+x ~/.local/bin/stack
+~/.local/bin/stack --system-ghc --resolver nightly-2020-06-01 install \
+  alex \
+  happy \
+  hscolour
+
+cd /tmp
+git clone --recurse-submodules --branch $BRANCH https://github.com/TerrorJack/ghc.git
+cd /asterius
+
+export PATH=~/.local/bin:$PATH
+mv .github/workflows/build-linux.mk /tmp/ghc/mk/build.mk
+cd /tmp/ghc
+./boot
+./configure --disable-ld-override
+make
+make binary-dist
+mkdir ghc-bindist
+mv *.tar.* ghc-bindist/
+(ls -l ghc-bindist && sha256sum -b ghc-bindist/*) > ghc-bindist/sha256.txt
+cd /asterius
+
+mv /tmp/ghc/ghc-bindist .
---
+++ .github/workflows/build-tinfo6.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo 'deb [check-valid-until=no] http://snapshot.debian.org/archive/debian/20200602T145955Z sid main contrib non-free' > /etc/apt/sources.list
+apt update
+apt full-upgrade -y
+apt install -y \
+  automake \
+  build-essential \
+  curl \
+  gawk \
+  git \
+  libffi-dev \
+  libgmp-dev \
+  libncurses-dev \
+  libnuma-dev \
+  libtool-bin \
+  pkg-config \
+  python3-sphinx \
+  xz-utils \
+  zlib1g-dev
+mkdir -p ~/.local/bin
+curl -L https://github.com/commercialhaskell/stack/releases/download/v2.3.1/stack-2.3.1-linux-x86_64-bin -o ~/.local/bin/stack
+chmod u+x ~/.local/bin/stack
+~/.local/bin/stack --resolver nightly-2020-06-01 install \
+  alex \
+  happy \
+  hscolour
+
+pushd /tmp
+git clone --recurse-submodules --branch $BRANCH https://github.com/TerrorJack/ghc.git
+popd
+
+export PATH=~/.local/bin:$(~/.local/bin/stack path --compiler-bin):$PATH
+mv .github/workflows/build-linux.mk /tmp/ghc/mk/build.mk
+pushd /tmp/ghc
+./boot
+./configure
+make
+make binary-dist
+mkdir ghc-bindist
+mv *.tar.* ghc-bindist/
+(ls -l ghc-bindist && sha256sum -b ghc-bindist/*) > ghc-bindist/sha256.txt
+popd
+
+mv /tmp/ghc/ghc-bindist .
